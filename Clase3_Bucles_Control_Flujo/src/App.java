80d8dc47b629c0c8d9baeda8c0c3bb7b04bde753
Darkot1 <sebarango136@gmail.com>
2024-07-11 21:20:27 -0500

    clase 3

--- Clase3_Bucles_Control_Flujo/src/App.java
+++ Clase3_Bucles_Control_Flujo/src/App.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args){
-        System.out.println("Clase 3 Bucles y control de flujo");
+    public static void main(String[] args) {
+        String abc = "abcdefghijklmnñopqrstuvwxyz";
+        abc.toCharArray();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa tu frase a cifrar:");
+        String frase = scanner.nextLine().toLowerCase();
+
+        System.out.println("Ingrese su clave (en número):");
+        int clave = scanner.nextInt();
+
+        scanner.close();
+
+        String mensajeCifrado = cifrar(frase, clave, abc);
+        System.out.println("Mensaje Cifrado: " + mensajeCifrado);
     }
+
+    // Método para cifrar el mensaje
+    public static String cifrar(String mensaje, int desplazamiento, String abecedario) {
+        char[] cifrado = new char[mensaje.length()];
+
+        for (int i = 0; i < mensaje.length(); i++) {
+            char caracter = mensaje.charAt(i);
+            int posicionOriginal = abecedario.indexOf(caracter);
+            int nuevaPosicion = (posicionOriginal + desplazamiento) % abecedario.length();
+            cifrado[i] = abecedario.charAt(nuevaPosicion);
+        }
+        return new String(cifrado);
+}
 }
